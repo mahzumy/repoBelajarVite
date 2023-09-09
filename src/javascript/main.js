38dc6f112830634b99bdf587ad8d790408d0195b
mahzumy <mahzumy39@gmail.com>
2023-09-09 12:10:28 +0700

    edit final

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,7 +1,7 @@
 import Typed from "typed.js";
 
 new Typed ("#text1", {
-    strings : ["Saya belajar membuat Persolan Website"],
+    strings : ["Saya belajar membuat Personal Website"],
     typeSpeed : 50,
     backSpeed : 70,
     loop : true,
